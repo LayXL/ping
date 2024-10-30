6b2c174241de7cc4077e625ce1bbf54c4ba4a522
Виталий <46228376+LayXL@users.noreply.github.com>
2024-10-30 15:53:36 +0500

    leaderboard

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { t } from "../trpc"
 
 import * as game from "./game"
 import { healthcheck } from "./healthcheck"
+import * as leaderboard from "./leaderboard"
 
 export const router = t.router({
   healthcheck,
   game,
+  leaderboard,
 })
 
 export type Router = typeof router
