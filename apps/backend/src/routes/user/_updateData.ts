fe1009db4762dc8183800edcadaa9b7ffec6713c
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-12 23:12:04 +0500

    fix

--- apps/backend/src/routes/user/_updateData.ts
+++ apps/backend/src/routes/user/_updateData.ts
@@ -1,5 +1,6 @@
 import { TRPCError } from "@trpc/server"
 import { db } from "drizzle"
+import { eq } from "drizzle-orm"
 import { users } from "drizzle/schema.ts"
 import { returnFirst } from "shared/returnFirst.ts"
 import { privateProcedure } from "../../trpc.ts"
@@ -16,12 +17,15 @@ export const updateData = privateProcedure.query(async ({ ctx }) => {
   if (!user)
     throw new TRPCError({ code: "NOT_FOUND", message: "User not found" })
 
-  await db.update(users).set({
-    displayName: `${user.first_name} ${user.last_name}`,
-    firstName: user.first_name,
-    lastName: user.last_name,
-    avatarUrl: user.photo_max_orig,
-  })
+  await db
+    .update(users)
+    .set({
+      displayName: `${user.first_name} ${user.last_name}`,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      avatarUrl: user.photo_max_orig,
+    })
+    .where(eq(users.id, ctx.user.id))
 
   return true
 })
